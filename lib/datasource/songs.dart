f77ba4485dce5ee38887068da88426391073bb35
Iru <48447514@est.ort.edu.ar>
2025-06-01 23:37:09 -0300

    claro?

--- lib/datasource/songs.dart
+++ lib/datasource/songs.dart
@@ -6,55 +6,90 @@ final List<Song> songs = [
     title: 'Something Stupid Called Love',
     singer: 'Frank Sinatra & Nancy Sinatra',
     year: 1967,
-    posterUrl:
-        'https://upload.wikimedia.org/wikipedia/en/9/97/Something_Stupid_single_cover.jpg',
+    posterUrl: 'https://t2.genius.com/unsafe/128x128/https%3A%2F%2Fimages.genius.com%2F4d5ed1e98b9ddba86948f73acd273fbe.1000x1000x1.png',
   ),
   Song(
     id: '2',
     title: 'Walk',
     singer: 'NCT 127',
     year: 2023,
-    posterUrl:
-        'https://i.scdn.co/image/ab67616d0000b2732c03c8bfe216bd52c1aa6d09',
+    posterUrl: 'https://t2.genius.com/unsafe/258x258/https%3A%2F%2Fimages.genius.com%2Fcd4f59977d093858a464007db08b750c.1000x1000x1.png',
   ),
   Song(
     id: '3',
     title: 'Bite Me',
     singer: 'Enhypen',
     year: 2023,
-    posterUrl:
-        'https://i.scdn.co/image/ab67616d0000b273cc15e3c58d0021d8ac6a8914',
+    posterUrl: 'https://t2.genius.com/unsafe/128x128/https%3A%2F%2Fimages.genius.com%2Fe64d65b1cce142a7b7f6c77ff71f1613.1000x1000x1.png',
   ),
   Song(
     id: '4',
     title: 'Ice on My Teeth',
     singer: 'ATEEZ',
     year: 2023,
-    posterUrl:
-        'https://i.scdn.co/image/ab67616d0000b27379c222ecae7ec7ed6d22427f',
+    posterUrl: 'https://t2.genius.com/unsafe/128x128/https%3A%2F%2Fimages.genius.com%2Fc669159af31316e11f4cd3fc5f28dd63.1000x1000x1.png',
   ),
   Song(
     id: '5',
     title: 'Freak on a Leash',
     singer: 'Korn',
     year: 1998,
-    posterUrl:
-        'https://upload.wikimedia.org/wikipedia/en/5/55/Korn_Freak_on_a_Leash.jpg',
+    posterUrl: 'https://t2.genius.com/unsafe/128x128/https%3A%2F%2Fimages.genius.com%2F9e75d0bf421fc3a68afb7f89a02a0741.1000x1000x1.jpg',
   ),
   Song(
     id: '6',
     title: 'Twist',
     singer: 'Korn',
     year: 1996,
-    posterUrl:
-        'https://upload.wikimedia.org/wikipedia/en/0/02/Korn_-_Life_Is_Peachy.jpg',
+    posterUrl: '',
   ),
   Song(
     id: '7',
-    title: 'We Don\'t Talk Anymore',
+    title: 'We Dont Talk Anymore',
     singer: 'Charlie Puth feat. Selena Gomez',
     year: 2016,
-    posterUrl:
-        'https://upload.wikimedia.org/wikipedia/en/3/38/Charlie_Puth_-_We_Don%27t_Talk_Anymore.png',
+    posterUrl: 'https://t2.genius.com/unsafe/128x128/https%3A%2F%2Fimages.genius.com%2F40925f18b6a08542a0d8bc01fa37a897.999x999x1.png',
+  ),
+  Song(
+    id: '8',
+    title: 'Cachita',
+    singer: 'Lisa Ono',
+    year: 2005,
+    posterUrl: 'https://images.genius.com/f739c489c7ca24791f1e26bb1da8d194.1000x1000x1.jpg',
+  ),
+  Song(
+    id: '9',
+    title: 'El Pollito Pío',
+    singer: 'Los Meñiques de la Casa',
+    year: 2012,
+    posterUrl: 'https://t2.genius.com/unsafe/128x128/https%3A%2F%2Fimages.genius.com%2Fddc5c332478b4b789a65fe89be6ef69c.429x429x1.jpg',
+  ),
+  Song(
+    id: '10',
+    title: 'Back To Me',
+    singer: 'The Rose',
+    year: 2023,
+    posterUrl: 'https://t2.genius.com/unsafe/128x128/https%3A%2F%2Fimages.genius.com%2F18c98d105ce1a0d24e84a4b23edc8d2b.1000x1000x1.jpg',
+  ),
+  Song(
+    id: '11',
+    title: 'Treat You Better',
+    singer: 'Shawn Mendes',
+    year: 2016,
+    posterUrl: 'https://t2.genius.com/unsafe/128x128/https%3A%2F%2Fimages.genius.com%2F1fd1d287ceba41c302525e8de8e3429a.1000x1000x1.png',
+  ),
+  Song(
+    id: '12',
+    title: 'Sorry',
+    singer: 'Justin Beiber',
+    year: 2015,
+    posterUrl: 'https://t2.genius.com/unsafe/128x128/https%3A%2F%2Fimages.genius.com%2Fddab64aa5e55030c98e4979aef0bea20.1000x1000x1.png',
+  ),
+  Song(
+    id: '13',
+    title: 'Daylight',
+    singer: 'David Kushner',
+    year: 2023,
+    posterUrl: 'https://akamai.sscdn.co/uploadfile/letras/fotos/7/0/c/2/70c24a8dc8728363c7ae7a7c223a14fe.jpg',
   ),
 ];
