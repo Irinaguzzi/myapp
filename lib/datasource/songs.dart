f8fc79dec3fe95731a72f9027666722513b2dded
Iru <48447514@est.ort.edu.ar>
2025-06-01 23:40:44 -0300

    Update songs.dart

--- lib/datasource/songs.dart
+++ lib/datasource/songs.dart
@@ -57,4 +57,46 @@ final List<Song> songs = [
     posterUrl:
         'https://upload.wikimedia.org/wikipedia/en/3/38/Charlie_Puth_-_We_Don%27t_Talk_Anymore.png',
   ),
+  Song(
+    id: '8',
+    title: 'Cachita',
+    singer: 'Lisa Ono',
+    year: 2005,
+    posterUrl: 'https://images.genius.com/f739c489c7ca24791f1e26bb1da8d194.1000x1000x1.jpg',
+  ),
+  Song(
+    id: '9',
+    title: 'El Pollito Pío',
+    singer: 'Los Meñiques de la Casa',
+    year: 2012,
+    posterUrl: 'https://t2.genius.com/unsafe/128x128/https%3A%2F%2Fimages.genius.com%2Fddc5c332478b4b789a65fe89be6ef69c.429x429x1.jpg',
+  ),
+  Song(
+    id: '10',
+    title: 'Back To Me',
+    singer: 'The Rose',
+    year: 2023,
+    posterUrl: 'https://t2.genius.com/unsafe/128x128/https%3A%2F%2Fimages.genius.com%2F18c98d105ce1a0d24e84a4b23edc8d2b.1000x1000x1.jpg',
+  ),
+  Song(
+    id: '11',
+    title: 'Treat You Better',
+    singer: 'Shawn Mendes',
+    year: 2016,
+    posterUrl: 'https://t2.genius.com/unsafe/128x128/https%3A%2F%2Fimages.genius.com%2F1fd1d287ceba41c302525e8de8e3429a.1000x1000x1.png',
+  ),
+  Song(
+    id: '12',
+    title: 'Sorry',
+    singer: 'Justin Beiber',
+    year: 2015,
+    posterUrl: 'https://t2.genius.com/unsafe/128x128/https%3A%2F%2Fimages.genius.com%2Fddab64aa5e55030c98e4979aef0bea20.1000x1000x1.png',
+  ),
+  Song(
+    id: '13',
+    title: 'Daylight',
+    singer: 'David Kushner',
+    year: 2023,
+    posterUrl: 'https://akamai.sscdn.co/uploadfile/letras/fotos/7/0/c/2/70c24a8dc8728363c7ae7a7c223a14fe.jpg',
+  ),
 ];
